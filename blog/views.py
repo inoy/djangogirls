fcf7d2f5baa23da640d40d5734937a19de44b6f7
inoy <7m7110123@gmail.com>
2023-06-22 11:05:09 +0900

    mypy, Pylance, isort

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import AnonymousUser
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
@@ -5,20 +8,20 @@ from .forms import PostForm
 from .models import Post
 
 
-def post_list(request):
+def post_list(request: HttpRequest) -> HttpResponse:
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by(
         "published_date"
     )
     return render(request, "blog/post_list.html", {"posts": posts})
 
 
-def post_detail(request, pk):
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
     return render(request, "blog/post_detail.html", {"post": post})
 
 
-def post_new(request):
+def post_new(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -32,12 +35,14 @@ def post_new(request):
     return render(request, "blog/post_edit.html", {"form": form})
 
 
-def post_edit(request, pk):
+@login_required
+def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
+            assert not isinstance(request.user, AnonymousUser)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
